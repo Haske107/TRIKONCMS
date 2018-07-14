f7fc9b0a5d0dbc092445423e30662d9d0f746c8e
Jeff <Haske107@mail.chapman.edu>
2018-07-13 20:00:07 -0500

    Added Router

---
+++ src/app/main/client/client-search-page/client-search-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClientSearchPageComponent } from './client-search-page.component';
+
+describe('ClientSearchPageComponent', () => {
+  let component: ClientSearchPageComponent;
+  let fixture: ComponentFixture<ClientSearchPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ClientSearchPageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ClientSearchPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/main/client/client.module.ts
+++ src/app/main/client/client.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClientSearchPageComponent } from './client-search-page/client-search-page.component';
+import {ClientProfilePageComponent} from "./client-search-page/client-profile-page/client-profile-page.component";
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    ClientProfilePageComponent,
+    ClientSearchPageComponent
+  ]
 })
 export class ClientModule { }
