4f2178d04e64ee663a6e945272645235d89b26e5
Jeff <Haske107@mail.chapman.edu>
2018-08-08 20:04:10 -0700

    s"

--- Data Management/DB-Backup.js
+++ Data Management/DB-Backup.js
@@ -2,39 +2,53 @@ const ncp = require('ncp').ncp;
 const fs = require('fs');
 const CronJob = require('cron').CronJob;
 
-// DEFINE CRON JOB
-const job = new CronJob('*/10 * * * * *', () => {
-    console.log("Starting Back Up Sequence: " + new Date);
-    backUpDB(new Date);
-    console.log("Back Up Sequence Complete");
-  }, function () {}, true
-);
+// // DEFINE CRON JOB
+// const job = new CronJob('*/10 * * * * *', () => {
+//     console.log("Starting Back Up Sequence: " + new Date);
+//     backUpDB(new Date);
+//     console.log("Back Up Sequence Complete");
+//   }, function () {}, true
+// );
+//
+// // DEFINE BACK UP
+// function backUpDB(date) {
+//
+//
+// }
+//
+// // BEGIN PROCESS
+// job.start();
 
-// DEFINE BACK UP
-function backUpDB(date) {
-  const DB = 'G:\\';
-  const Destination = 'E:\\';
-  if (fs.existsSync(DB))  {
-    console.log("Found the DB");
-    if (fs.existsSync(Destination))  {
-      console.log("Found the Destination");
-    } else  {
-      console.log("Can't Find the Destination");
-      fs.mkdirSync(Destination);
-      console.log("Created the Destination");
-    }
-    ncp(DB, Destination, function (err) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('Successfully Backed Up DB: Task Time - ' + (new Date - date) + ' Seconds');
-      }
+
+// GET PROJECT PATCHES
+function getProjectPatches() {
+  try {
+
+    let Archive = new FSTree({
+      entries: walkSync.entries('E:\\')
     });
-  } else  {
-    console.log("Can't Find the DB")
+
+    let EditingDrivesTree = new FSTree({
+      entries: walkSync.entries('U:\\')
+    });
+
+    let ValidPatches = [];
+
+    for (let Patch of EditingDrivesTree.calculatePatch(Archive)) {
+
+      ValidPatches.push(Patch);
+
+    }
+
+    return ValidPatches;
+
+  } catch (error) {
+
+    console.log("ERROR OCCURED!", error);
+    return [];
+
   }
 }
 
-// BEGIN PROCESS
-job.start();
 
+cosole.log(getProjectPatches());
